015c982856fbd20e05144fcd67cd31f5ff540bee
l1ghth4t <pirogov30002@gmail.com>
2024-08-27 06:57:28 +0300

    publish version 0.0.11

--- deploy.ps1
+++ deploy.ps1
@@ -10,7 +10,7 @@ $patch = [int]$Matches[1] + 1
 $version = "$($major).$($minor).$($patch)"
 
 Write-Host 'The new version is: ' $version
-$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $True
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 [System.IO.File]::WriteAllLines("VERSION", $version, $Utf8NoBomEncoding)
 
 git add .
